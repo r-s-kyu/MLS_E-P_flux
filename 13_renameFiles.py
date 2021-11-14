263db1c8caa91c1aea72f04a1440b66690d48d13
Riku Satake <r.s.kyushu@gmail.com>
2021-11-15 04:42:32 +0900

    rename file

--- 13_renameFiles.py
+++ 13_renameFiles.py
@@ -2,8 +2,7 @@
 import os
 import calendar
 # ====================初期値===================
-startyear = 2010
-endyear = 2020
+year = 2020
 
 # ====================描画値===================
 vector_scale = 8.0e+5
@@ -14,12 +13,9 @@ ylabel=(["100", "50", "10", "5", "1", "0.1"])
 latrange = [-80,-30]
 
 def main():
-    for year in range(startyear,endyear+1):
-        for month in range(1,13):
-            for day in range(1,calendar.monthrange(year,month)[1]+1):
-                dates = f'{str(year).zfill(4)+str(month).zfill(2)+str(day).zfill(2)}'
-                newname = f'D:/data/MLS/e-p_flux/{year}/e-p_flux.{dates}.npz'
-                oldname = f'D:/data/MLS/e-p_flux/{year}/e-p_flux.{dates}.nyz.npz'
-                os.rename(oldname,newname)
+    for month in range(1,13):
+        newname = f'D:/picture/study/MLS/monthYearMean/zonalU/month{month}Mean_2010to2019and{year}_E-Pflux_zonalU.png'
+        oldname = f'D:/picture/study/MLS/monthYearMean/zonalU/month{month}Mean_2010to2019and{year}_E-Pflux_zonalT.png'
+        os.rename(oldname,newname)
 
 main()
